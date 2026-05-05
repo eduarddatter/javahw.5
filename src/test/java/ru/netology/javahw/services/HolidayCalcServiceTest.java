ccd4c0d4de550230509f0cdce0fad476b2c295c2
kristina test <msalechristina@gmail.com>
2026-05-05 07:54:44 +0300

    ParameterizedTest add

--- src/test/java/ru/netology/javahw/services/HolidayCalcServiceTest.java
+++ src/test/java/ru/netology/javahw/services/HolidayCalcServiceTest.java
@@ -2,15 +2,23 @@ package ru.netology.javahw.services;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class HolidayCalcServiceTest {
-    @Test
-    public void shouldCalculateForThreeMonthsHoliday() {
+    @ParameterizedTest
+//    @CsvSource({
+//            "10000,3000,20000,3",
+//            "100000,60000,150000,2"
+//    })
+    @CsvFileSource(files = "src/test/resources/holiday.csv")
+    public void shouldCalculateForThreeMonthsHoliday(int income, int expenses, int threshold, int expected) {
         HolidayCalcService service = new HolidayCalcService();
-        int income = 10_000;
-        int expenses = 3_000;
-        int threshold = 20_000;
-        int expected = 3;
+        //int income = 10_000;
+        //int expenses = 3_000;
+        //int threshold = 20_000;
+        //int expected = 3;
         int actual = service.calculate(income, expenses, threshold);
         Assertions.assertEquals(expected, actual);
     }
